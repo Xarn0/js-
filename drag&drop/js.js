789523c86a906477a1b19d0b807437ebf108a02d
xarn0 <xouli@list.ru>
2025-05-21 00:40:29 +0300

    drag & drop

---
+++ drag&drop/js.js
@@ -0,0 +1,31 @@
+const el = document.querySelectorAll(".draggable");
+
+el.forEach((item) => {
+	let isDragging = false;
+	let offsetX = 0;
+	let offsetY = 0;
+
+	item.addEventListener("mousedown", (e) => {
+		isDragging = true;
+		offsetX = e.clientX - item.offsetLeft;
+		console.log("clientx " + e.clientX);
+		console.log("offsetLeft " + item.offsetLeft);
+
+		offsetY = e.clientY - item.offsetTop;
+		item.style.cursor = "grabbing";
+	});
+
+	document.addEventListener("mousemove", (e) => {
+		if (!isDragging) return;
+
+		item.style.left = `${e.clientX - offsetX}px`;
+		item.style.top = `${e.clientY - offsetY}px`;
+	});
+
+	document.addEventListener("mouseup", () => {
+		if (isDragging) {
+			isDragging = false;
+			item.style.cursor = "grab";
+		}
+	});
+});
